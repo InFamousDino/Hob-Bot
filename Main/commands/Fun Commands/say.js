73f1953d1067a3c1888b1222e2bcfebc1d44c5b4
Seb <61729130+InFamousDino@users.noreply.github.com>
2021-02-16 09:59:41 +1100

    accidently did something wrong

--- Main/commands/Fun Commands/say.js
+++ Main/commands/Fun Commands/say.js
@@ -1,13 +1,29 @@
-if(!args[0]) return message.channel.send(`what am i meant to say?!!?!?`)
-const toSend = args.join(" ")
-const banned = [
-    "@everyone",
-    "@here",
-    "<@&" //roles
-]
-let illegalmessage = false
-banned.forEach( msg => {
-    if(toSend.includes(msg)) illegalmessage = true
-})
-if(illegalmessage == true) return message.channel.send("no! ❤")
-message.channel.send(toSend)
+const filename = require('path').basename(__filename).split(".")[0]
+exports.execute = (client, message, args) => {
+    if(!args[0]) return message.channel.send(`what am i meant to say?!!?!?`)
+    const toSend = args.join(" ")
+    const banned = [
+        "@everyone",
+        "@here",
+        "<@&" //roles
+    ]
+    let illegalmessage = false
+    banned.forEach( msg => {
+        if(toSend.includes(msg)) illegalmessage = true
+    })
+    if(illegalmessage == true) return message.channel.send("no! ❤")
+    message.channel.send(toSend)
+}
+exports.config = {
+    disabled: true, // if the command is disabled
+    permission: [], // List of perms https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
+    guildOnly: false, // if you can use commands in only guilds or also dms
+    hobDevOnly: false, // If you want only hob devs can use
+    aliases: []
+}
+exports.info = {
+    name: filename,
+    category: __dirname.split("/")[__dirname.split("/").length - 1],
+    description: `make hob say something`,
+    usage: `${filename} <message>` 
+}
