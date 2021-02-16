91c1c4223e0eb8940802ebfec7449de3c4d4a89a
Seb <61729130+InFamousDino@users.noreply.github.com>
2021-02-16 18:03:50 +1100

    fixed role removal in mute

--- Main/commands/Administrative_Commands/mute.js
+++ Main/commands/Administrative_Commands/mute.js
@@ -10,18 +10,15 @@ exports.execute = (client, message, args) => {
       let memberTarget = message.guild.members.cache.get(target.id);
 
       if (!args[1]) {
-          memberTarget.roles.remove(mainRole.id);
           memberTarget.roles.add(muteRole.id);
           message.channel.send(`<@${memberTarget.user.id}> has been muted`);
           return
       }
       memberTarget.roles.remove(mainRole.id);
-      memberTarget.roles.add(muteRole.id);
       message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
 
       setTimeout(function () {
           memberTarget.roles.remove(muteRole.id);
-          memberTarget.roles.add(mainRole.id);
       }, ms(args[1]));
   } else {
       message.channel.send('Cant find that member!');
