c3c23a92950cb1d824a7248a1e02026e958fccd1
Seb <61729130+InFamousDino@users.noreply.github.com>
2021-02-16 17:59:52 +1100

    Added mute and updated license

---
+++ Main/commands/Administrative_Commands/mute.js
@@ -0,0 +1,44 @@
+const filename = require('path').basename(__filename).split(".")[0]
+exports.execute = (client, message, args) => {
+  const ms = require('ms')
+  const target = message.mentions.users.first();
+  if (target) {
+
+      let mainRole = message.guild.roles.cache.find(role => role.name === 'Cool People');
+      let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+
+      let memberTarget = message.guild.members.cache.get(target.id);
+
+      if (!args[1]) {
+          memberTarget.roles.remove(mainRole.id);
+          memberTarget.roles.add(muteRole.id);
+          message.channel.send(`<@${memberTarget.user.id}> has been muted`);
+          return
+      }
+      memberTarget.roles.remove(mainRole.id);
+      memberTarget.roles.add(muteRole.id);
+      message.channel.send(`<@${memberTarget.user.id}> has been muted for ${ms(ms(args[1]))}`);
+
+      setTimeout(function () {
+          memberTarget.roles.remove(muteRole.id);
+          memberTarget.roles.add(mainRole.id);
+      }, ms(args[1]));
+  } else {
+      message.channel.send('Cant find that member!');
+  }
+}
+
+exports.config = {
+  disabled: false, // if the command is disabled
+  permission: ['ADMINISTRATOR'], // List of perms https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
+  guildOnly: true, // if you can use commands in only guilds or also dms
+  hobDevOnly: false, // If you want only hob devs can use
+  aliases: []
+}
+
+exports.info = {
+  name: filename,
+  category: __dirname.split("/")[__dirname.split("/").length - 1],
+  description: `Mute a user.`,
+  usage: `${filename} <@user> <Time>` 
+}
---
+++ Main/commands/Administrative_Commands/unmute.js
@@ -0,0 +1,31 @@
+const filename = require('path').basename(__filename).split(".")[0]
+exports.execute = (client, message, args) => {
+  const target = message.mentions.users.first();
+        if(target){
+            let mainRole = message.guild.roles.cache.find(role => role.name === 'Cool People');
+            let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
+ 
+            let memberTarget= message.guild.members.cache.get(target.id);
+ 
+            memberTarget.roles.remove(muteRole.id);
+            memberTarget.roles.add(mainRole.id);
+            message.channel.send(`<@${memberTarget.user.id}> has been unmuted`);
+        } else{
+            message.channel.send('Cant find that member!');
+        }
+}
+
+exports.config = {
+  disabled: false, // if the command is disabled
+  permission: ['ADMINISTRATOR'], // List of perms https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
+  guildOnly: true, // if you can use commands in only guilds or also dms
+  hobDevOnly: false, // If you want only hob devs can use
+  aliases: []
+}
+
+exports.info = {
+  name: filename,
+  category: __dirname.split("/")[__dirname.split("/").length - 1],
+  description: `Unmute a user.`,
+  usage: `${filename} <@user>` 
+}
