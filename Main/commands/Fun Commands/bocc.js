8a86cfe7a0b4937f0bf06f245659a46ae511a043
Seb <61729130+InFamousDino@users.noreply.github.com>
2021-02-21 00:23:35 +1100

    added amogus and bocc

---
+++ Main/commands/Fun Commands/bocc.js
@@ -0,0 +1,17 @@
+const filename = require('path').basename(__filename).split(".")[0]
+exports.execute = (client, message, args) => {
+    message.channel.send(`bad ${message.author} go into the box you. https://tenor.com/view/cat-box-quit-silence-angry-gif-12374805`)
+}
+exports.config = {
+    disabled: false, // if the command is disabled
+    permission: [], // List of perms https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
+    guildOnly: false, // if you can use commands in only guilds or also dms
+    hobDevOnly: false, // If you want only hob devs can use
+    aliases: []
+}
+exports.info = {
+    name: filename,
+    category: __dirname.split("/")[__dirname.split("/").length - 1],
+    description: `boccccccc`,
+    usage: `${filename}` 
+}
