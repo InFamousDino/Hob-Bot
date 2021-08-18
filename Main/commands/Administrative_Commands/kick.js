ee30be8099189e880a00051e51edff67f4bb2974
Seb <61729130+InFamousDino@users.noreply.github.com>
2021-08-18 19:28:26 +1000

    disabled ban and kick as of this time

--- Main/commands/Administrative_Commands/kick.js
+++ Main/commands/Administrative_Commands/kick.js
@@ -45,7 +45,7 @@ exports.execute = (client, message, args) => {
     message.channel.send({ embeds: [kicklog] })
 }
 exports.config = {
-    disabled: false, // if the command is disabled
+    disabled: true, // if the command is disabled
     permission: ["KICK_MEMBERS"], // List of perms https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
     guildOnly: true, // if you can use commands in only guilds or also dms
     hobDevOnly: false, // If you want only hob devs can use
